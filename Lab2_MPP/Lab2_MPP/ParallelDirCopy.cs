98c901aab3813057cf14b4dece14cae1a856f23f
voyager-01 <sovereign.stas@yandex.ru>
2017-09-14 18:07:46 +0300

    Data check added.
    Data check added.

---
+++ Lab2_MPP/Lab2_MPP/ParallelDirCopy.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+
+namespace Lab2_MPP
+{
+    public class ParallelDirCopy
+    {
+        private string origin_path;
+        private string target_path;
+        private int threads_capacity;
+
+        private string Origin_Path
+        {
+            get
+            {
+                return origin_path;
+            }
+            set
+            {
+                if (Directory.Exists(value))
+                {
+                    origin_path = value;
+                }
+                else
+                {
+                    origin_path = null;
+                    throw new DirectoryNotFoundException("Папка " + value + "не существует");
+                }
+            }
+        }
+
+        private string Target_Path
+        {
+            get
+            {
+                return target_path;
+            }
+            set
+            {
+                if (Directory.Exists(value))
+                {
+                    target_path = value;
+                }
+                else
+                {
+                    target_path = null;
+                    throw new DirectoryNotFoundException("Папка " + value + " не существует");
+                }
+            }
+        }
+
+
+        public ParallelDirCopy(string origin, string target, int capacity)
+        {
+            if (origin != target)
+            {
+                threads_capacity = capacity;
+                Origin_Path = origin;
+                Target_Path = target;
+            }
+            else
+                throw new DirectoryNotFoundException("Адрес исходного и целевого каталога идентичен.");
+        }
+
+        public void Execute()
+        {
+            //Выполнение
+        }
+
+        public string GetInfo()
+        {
+            //Формат вывода
+            return "";
+        }
+    }
+}
--- Lab2_MPP/Lab2_MPP/Program.cs
+++ Lab2_MPP/Lab2_MPP/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace Lab2_MPP
+﻿using System;
+using System.IO;
+
+namespace Lab2_MPP
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            string original_dir = "C:\\MPP2\\origin";
+            string target_dir = "C:\\MPP2\\target";
+            Console.Write("Количество потоков в пуле - ");
+            try
+            {
+                var pDirCopy = new ParallelDirCopy(original_dir, target_dir, int.Parse(Console.ReadLine()));
+                pDirCopy.Execute();
+                Console.WriteLine(pDirCopy.GetInfo());
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Количество пулов в потоке слишком велико.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Количество заданно некорректно.");
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
         }
     }
 }
